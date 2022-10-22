f5318d84ef5440dc2c6ad609ccc8f1c416132243
Steckij Nikita <stets119@gmail.com>
2022-10-22 15:44:49 +0300

    user authorization done

--- index.js
+++ index.js
@@ -14,8 +14,46 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Неверный логин или пароль',
+            });
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: 'Неверный логин или пароль',
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            'secret2022',
+            {
+                expiresIn: '30d',
+            },
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось авторизоваться',
+        });
+    }
 });
 
 app.post('/auth/register', registerValidation, async (req, res) => {
@@ -60,6 +98,8 @@ app.post('/auth/register', registerValidation, async (req, res) => {
         });
     }
 });
+
+
 
 app.listen(4444, (err) => {
     if (err) {
